2972701b80e4f81068b0b284e4e4fd6fcd5745a4
Parth Doshi <144593310+parthdoshi7@users.noreply.github.com>
2025-07-13 09:48:16 +0530

    Delete middleware directory

--- middleware/admin.js
+++
@@ -1,9 +0,0 @@
-const admin = (req, res, next) => {
-    // Check if user exists and is an admin
-    if (!req.user || req.user.role !== 'admin') {
-        return res.status(403).json({ msg: 'Access denied. Admin privileges required.' });
-    }
-    next();
-};
-
-module.exports = admin; 
